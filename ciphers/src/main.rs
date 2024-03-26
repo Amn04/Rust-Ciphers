eb38776d5c57af39dc88987e362d71f9b451c423
Amn04 <abk8088@gmail.com>
2024-03-26 19:36:03 +0530

    Added skeleton

--- ciphers/src/main.rs
+++ ciphers/src/main.rs
@@ -1,3 +1,15 @@
+use clap::{command, Parser};
+use clap_derive::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[derive(Debug)]
+struct Cli {
+    #[arg(short, long)]
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    
 }
